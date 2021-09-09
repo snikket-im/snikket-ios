991c4e41b6ce7d54acbd42ba7965ec543f4826b4
Muhammad Khalid <muhammadkhalid620@gmail.com>
2021-09-09 18:45:41 +0500

    - Implemented XEP0329

--- Snikket/ui/AvatarColors.swift
+++ Snikket/ui/AvatarColors.swift
@@ -9,96 +9,42 @@
 import Foundation
 import UIKit
 import TigaseSwift
-import CommonCrypto
+import HSLuvSwift
 
 class AvatarColors {
-    
-    static let colors = [
-                    "0xFFE91E63", //pink 500
-                    "0xFFD81B60", //pink 600
-                    "0xFFC2185B", //pink 700
-                    "0xFFAD1457", //pink 800
-
-                    "0xFF9C27B0", //purple 500
-                    "0xFF8E24AA", //purple 600
-                    "0xFF7B1FA2", //purple 700
-                    "0xFF6A1B9A", //purple 800
-
-                    "0xFF673AB7", //deep purple 500,
-                    "0xFF5E35B1", //deep purple 600
-                    "0xFF512DA8", //deep purple 700
-                    "0xFF4527A0", //deep purple 800,
-
-                    "0xFF3F51B5", //indigo 500,
-                    "0xFF3949AB",//indigo 600
-                    "0xFF303F9F",//indigo 700
-                    "0xFF283593", //indigo 800
-
-                    "0xFF2196F3", //blue 500
-                    "0xFF1E88E5", //blue 600
-                    "0xFF1976D2", //blue 700
-                    "0xFF1565C0", //blue 800
-
-                    "0xFF03A9F4", //light blue 500
-                    "0xFF039BE5", //light blue 600
-                    "0xFF0288D1", //light blue 700
-                    "0xFF0277BD", //light blue 800
-
-                    "0xFF00BCD4", //cyan 500
-                    "0xFF00ACC1", //cyan 600
-                    "0xFF0097A7", //cyan 700
-                    "0xFF00838F", //cyan 800
-
-                    "0xFF009688", //teal 500,
-                    "0xFF00897B", //teal 600
-                    "0xFF00796B", //teal 700
-                    "0xFF00695C", //teal 800,
-
-                    "0xFF9E9D24", //lime 800
-
-                    "0xFF795548", //brown 500,
-        
-                    "0xFF607D8B", //blue grey 500,
-        
-                    // Unsafe Colors
-        
-                    "0xFFF44336", //red 500
-                    "0xFFE53935", //red 600
-                    "0xFFD32F2F", //red 700
-                    "0xFFC62828", //red 800
-
-                    "0xFFEF6C00", //orange 800
-
-                    "0xFFF4511E", //deep orange 600
-                    "0xFFE64A19", //deep orange 700
-                    "0xFFD84315" //deep orange 800,
-    ]
     
     static func getColorForName(name: String) -> UIColor {
             
         if name == "" {
             return UIColor(hex: "0xFF202020") ?? .gray
         }
-        let index = (getValueForName(name))
-        guard index < colors.count else { return .gray }
-        let colorHex = colors[Int(index)]
-        return UIColor(hex: colorHex) ?? .gray
-    }
-    
-    private static func getValueForName(_ name: String) -> Int64 {
-        guard let data: Data = Digest.md5.digest(data: name.data(using: .utf8)) else { return 0 }
         
-        //let signed = data.map { Int8(bitPattern: $0) }
-        
-        if var value = data.to(type: Int64.self) {
-            value = abs(value)
-            let modulo = value % Int64(colors.count)
-            return modulo
-        }
-        return 0
+        return XEP0392Helper.rgbFromNick(name: name)
     }
 }
 
+class XEP0392Helper {
+    
+    private static func angle(_ name: String) -> Double {
+        guard let data: Data = Digest.sha1.digest(data: name.data(using: .utf8)) else { return 0.0 }
+        
+        if data.count >= 2 {
+            let angle = Int(data[0] & 0xFF) + Int(data[1] & 0xFF) * 256
+            let value = Double(angle) / 65536.0
+            return value
+        }
+        return 0.0
+    }
+    
+    static func rgbFromNick(name: String) -> UIColor {
+        var hsluv : [Double] = []
+        hsluv.append(angle(name)*360)
+        hsluv.append(100)
+        hsluv.append(50)
+        
+        return UIColor(hue: hsluv[0], pastelSaturation: hsluv[1], lightness: hsluv[2], alpha: 1.0)
+    }
+}
 
 
 
